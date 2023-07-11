4d6c2bf780dfe5a85a88b2d687cff2ad6c6f0e58
AlejandraKeber <alejandrakeber@gmail.com>
2023-07-10 22:42:25 -0500

    Update random_greeting message

--- app/controllers/api/greetings_controller.rb
+++ app/controllers/api/greetings_controller.rb
@@ -1,6 +1,6 @@
 class Api::GreetingsController < ApplicationController
   def random_greeting
     greeting = Greeting.order("RANDOM()").first
-    render json: { greeting: greeting.text }
+    render json: greeting.text
   end
 end
